3b89898daf949e7bf4c6a40b8d6d91d7763e2331
whq611 <85942669+whq611@users.noreply.github.com>
2022-07-19 21:31:45 -0500

    Add files via upload

---
+++ l1328.java
@@ -0,0 +1,10 @@
+public class l1328 {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if(n<=1) return "";
+        for(int i=0; i<n/2; i++){
+            if(palindrome.charAt(i)!='a') return palindrome.substring(0,i) + 'a' + palindrome.substring(i+1,n);
+        }
+        return palindrome.substring(0,n-1) + 'b';
+    }
+}
